1ce74c2d0e4108d79a9e59671002c3d9423f3481
taha murat <ttahamuratdoser@gmail.com>
2022-12-27 20:10:49 +0300

    Initial Commit

---
+++ KentSimgeleri/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KentSimgeleri
+//
+//  Created by Taha  on 27.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

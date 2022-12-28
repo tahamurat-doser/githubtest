35146f6e413918ec3f164b264c64c83a9c263a01
taha murat <ttahamuratdoser@gmail.com>
2022-12-28 21:57:54 +0300

    no message

--- KentSimgeleri/ViewController.swift
+++ KentSimgeleri/ViewController.swift
@@ -7,13 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        
+        tableView.dataSource = self
+        
+        var kentSimgeleriNames = [String]()
+        kentSimgeleriNames.append("AniHarabeleri")
+        kentSimgeleriNames.append("Balikligol")
+        kentSimgeleriNames.append("KangalKopegi")
+        kentSimgeleriNames.append("Koprubası")
+        kentSimgeleriNames.append("NemrutDagı")
+        
+        var kentSimgeleriImages = [UIImage()]
+        kentSimgeleriImages.append(UIImage(named: "AniHarabeleri")!)
+        kentSimgeleriImages.append(UIImage(named: "Balikligol")!)
+        kentSimgeleriImages.append(UIImage(named: "KangalKopegi")!)
+        kentSimgeleriImages.append(UIImage(named: "Koprubası")!)
+        kentSimgeleriImages.append(UIImage(named: "NemrutDagı")!)
+        
+                                   
+                                   
+                                   
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { return 10
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        //cell.textLabel?.text = "test" şeklinde de yazılabilirdi
+        var content = cell.defaultContentConfiguration()
+        content.text = "Test"
+        //content.secondary = " yaz istedğini"
+        cell.contentConfiguration = content
+        return cell
+    }
+    
 }
 
